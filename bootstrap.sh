a1cee64739462cb8b9c638f2585196d2a5abf864
dcorbe <corbe@corbe.net>
2013-09-16 21:44:24 +0300

    Even more autotools decrufting.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,8 @@ then
 	make distclean
     fi
 
-    echo "rm -rf *~ *.scan aclocal.m4 *.cache *.log config.h *.in configure ltmain.sh config.*"
-          rm -rf *~ *.scan aclocal.m4 *.cache *.log config.h *.in configure ltmain.sh config.*
+    echo "rm -rf *~ *.scan aclocal.m4 *.cache *.log config.h *.in configure ltmain.sh config.* depcomp compile install-sh missing m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4"
+          rm -rf *~ *.scan aclocal.m4 *.cache *.log config.h *.in configure ltmain.sh config.* depcomp compile install-sh missing m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
 else
     autoreconf -fi
 fi
